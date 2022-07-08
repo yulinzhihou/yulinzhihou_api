187b195f73ff265190c75b8009c5edaee2425050
yulinzhihou <yulinzhihou@163.com>
2022-07-08 16:19:54 +0800

    修改推送脚本

--- push.sh
+++ push.sh
@@ -2,10 +2,8 @@
 # author yulinzhihou@gmail.com
 # data: 2022-07-09
 # 用于上传代码到代码仓库。提前建立好分支与线上的绑定关系
-# 参数1： 分支名： master
-# 参数2：提交代码的信息
+# 参数1：提交代码的信息
 
-[ -n $1 ] && "master";
 git add -A
-git commit -m "$2"
-git push origin "$1"
+git commit -m "$1"
+git push origin master
