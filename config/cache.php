6cc2e27799e3a8a298a627ee7d1da32346eea423
yulinzhihou <yulinzhihou@163.com>
2022-07-08 16:10:36 +0800

    框架必须支持缓存以及redis

--- config/cache.php
+++ config/cache.php
@@ -4,9 +4,11 @@
 // | 缓存设置
 // +----------------------------------------------------------------------
 
+use think\facade\Env;
+
 return [
     // 默认缓存驱动
-    'default' => env('cache.driver', 'file'),
+    'default' => env('cache.driver', 'redis'),
 
     // 缓存连接方式配置
     'stores'  => [
@@ -25,5 +27,13 @@ return [
             'serialize'  => [],
         ],
         // 更多的缓存连接
+        'redis' =>  [
+            'type'  =>  'redis',
+            'host'  =>  Env::get('redis.host','127.0.0.1'),
+            'pass'  =>  Env::get('redis.pass',''),
+            'port'  =>  Env::get('redis.port',6379),
+            'select'  =>  Env::get('redis.select',8),
+            'expire'  =>  Env::get('redis.expire',1440)
+        ]
     ],
 ];
